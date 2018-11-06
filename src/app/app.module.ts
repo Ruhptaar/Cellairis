408dc5ba4fa6e71bb85d2180f0e2970067e8a764
BigHomieKeel <rford@BigHomieKeels-MacBook-Air.local>
2018-11-05 19:33:16 -0600

    restart due to crash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -29,20 +29,19 @@ var config = {
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireStorageModule
+    IonicModule.forRoot(MyApp)
+    //AngularFireModule.initializeApp(firebaseConfig),
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage
   ],
   providers: [
     StatusBar,
