a41768c6fca3e10fafa15d34451d8b64a8c7fb02
BigHomieKeel <rford@hst000180858um.wifim1.olemiss.edu>
2018-11-07 09:10:50 -0600

    got interactive calendar added to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,26 +14,12 @@ import { SignupPage } from '../pages/signup/signup';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-/*import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-*/
+import { NgCalendarModule  } from 'ionic2-calendar';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { AuthProvider } from '../providers/auth/auth';
 import { EventProvider } from '../providers/event/event';
 import { ProfileProvider } from '../providers/profile/profile';
 import { AppointmentProvider } from '../providers/appointment/appointment';
-
-/*var config = {
-   apiKey: "AIzaSyDpW2mnEprnnlgUutR2WQ2UF6V4HKqLIU8",
-   authDomain: "cellairis-b2b0a.firebaseapp.com",
-   databaseURL: "https://cellairis-b2b0a.firebaseio.com",
-   projectId: "cellairis-b2b0a",
-   storageBucket: "cellairis-b2b0a.appspot.com",
-   messagingSenderId: "471928425313"
- };
-*/
 
 @NgModule({
   declarations: [
@@ -46,6 +32,7 @@ import { AppointmentProvider } from '../providers/appointment/appointment';
   ],
   imports: [
     BrowserModule,
+    NgCalendarModule,
     FormsModule,
     HttpModule,
     HttpClientModule,
